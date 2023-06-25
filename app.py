9646a6dec5baf0892d069661f148c86637c9a351
Frelin <fogariojohnson@gmail.com>
2023-06-25 11:47:01 -0500

    Designing the Routes and Data Structure

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    with open("user.json", "r") as file_obj:
+        blog_posts = json.loads(file_obj.read())
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
